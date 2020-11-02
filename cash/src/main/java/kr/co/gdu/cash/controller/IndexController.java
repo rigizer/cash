31f73e1546f52dd361da8011df31735dfa57ba9b
rigizer <rigizer@gmail.com>
2020-11-02 13:34:21 +0900

    index.jsp에 다이어리 날짜출력 기능 추가
    - NoticeService -> IndexService 이름 변경
    - IndexController에서 IndexService에 대한 객체명을 noticeService에서 indexService로
    이름 변경
    - index.jsp에서 JSTL을 이용하여 다이어리 날짜 출력
    - pom.xml dependency에 Spring DevTools 추가

--- cash/src/main/java/kr/co/gdu/cash/controller/IndexController.java
+++ cash/src/main/java/kr/co/gdu/cash/controller/IndexController.java
@@ -1,25 +1,52 @@
 package kr.co.gdu.cash.controller;
 
+import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import kr.co.gdu.cash.service.NoticeService;
+import kr.co.gdu.cash.service.IndexService;
 import kr.co.gdu.cash.vo.Notice;
 
 @Controller
 public class IndexController {
 	@Autowired
-	private NoticeService noticeService;
+	private IndexService indexService;
 	
 	@GetMapping(value={"/","/index"})
 	public String index(Model model) {
-		List<Notice> list = noticeService.getLatestNoticeList();
+		// request.getParameter("paramMonth", paramMonth);와 동일하다.
+		
+		// 1. 요청 분석
+		Calendar c = Calendar.getInstance();	// 오늘 날짜
+		int month = c.get(Calendar.MONTH) + 1;	// Calendar.MONTH에 1을 더해야 실제 월이 나온다.
+		int lastDay = c.getActualMaximum(Calendar.DATE); // 월 마지막 날짜
+		
+		System.out.println(month);
+		System.out.println(lastDay);
+
+		Calendar firstDay = Calendar.getInstance();		// 오늘 날짜
+		firstDay.set(Calendar.DATE, 1);	// 오늘 날짜 기준 일을 1로 바꾸어 이번 달 1일의 요일을 구한다.
+		int firstDayOfWeek = firstDay.get(Calendar.DAY_OF_WEEK);
+		System.out.println(firstDayOfWeek);
+		
+		// 2. 서비스 호출
+		List<Notice> list = indexService.getLatestNoticeList();
 		System.out.println("list.size(5) : " + (list.size() == 5));	// true
 		model.addAttribute("list", list);
+		
+		// 3. 뷰 연결
+		/*
+		 * 1) 월, 마지막 일(28일, 29일, 30일, 31일), 1일의 요일
+		 */
+		model.addAttribute("month", month);		// 월
+		model.addAttribute("lastDay", lastDay);	// 마지막 날
+		model.addAttribute("month", month);		// 1일의 요일
+		model.addAttribute("firstDayOfWeek", firstDayOfWeek);
 		return "index";	// 실제로 포워딩 되는 것은 prefix에서 지정한 /WEB-INF/view/index.jsp으로 반환됨
 	}
 }
--- cash/src/main/webapp/WEB-INF/view/index.jsp
+++ cash/src/main/webapp/WEB-INF/view/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -26,6 +26,47 @@
 						<td>${notice.noticeDate}</td>
 					</tr>
 				</c:forEach>
+			</tbody>
+		</table>
+		
+		<!-- 다이어리 -->
+		<h3>${month}월</h3>
+		<table border="1">
+			<thead>
+				<tr>
+					<th>일</th>
+					<th>월</th>
+					<th>화</th>
+					<th>수</th>
+					<th>목</th>
+					<th>금</th>
+					<th>토</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<c:forEach var="i" begin="1" end="${lastDay + (firstDayOfWeek - 1)}" step="1">
+						<c:if test="${i - (firstDayOfWeek - 1) < 1}">
+							<td>&nbsp;</td>
+						</c:if>
+						
+						<c:if test="${i - (firstDayOfWeek - 1) > 0}">
+							<td>${i - (firstDayOfWeek - 1)}</td>
+						</c:if>
+						
+						<c:if test="${i != (lastDay + (firstDayOfWeek - 1))}">
+							<c:if test="${i % 7 == 0}">
+								</tr><tr>
+							</c:if>
+						</c:if>
+					</c:forEach>
+					
+					<c:if test="${(lastDay + (firstDayOfWeek - 1)) % 7 != 0}">	
+						<c:forEach begin="1" end="${7 - ((lastDay + (firstDayOfWeek - 1)) % 7)}" step="1">	
+							<td>&nbsp;</td>	
+						</c:forEach>	
+					</c:if>
+				</tr>
 			</tbody>
 		</table>
 	</body>
